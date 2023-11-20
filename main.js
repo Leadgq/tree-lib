e71018adaf8bf04adedb5262f0f79d939a53e085
gaoqiang <gaoqiang@yundun119.com>
2023-11-20 16:23:29 +0800

    分发

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-import {
+export {
     calculatePercentage,
     getDistance,
     intersectionArray,
     symmetricDifference,
     difference,
 } from "./lib/math"
-import { getIntersection, getSymmetricDifference } from "./lib/tools"
-import {
+export {
+    getIntersection,
+    getSymmetricDifference
+} from "./lib/tools"
+
+export {
     isAbleArray,
     isAbleFn,
     isAbleObject,
@@ -16,7 +20,8 @@ import {
     reviseNumber,
     stringToNumber
 } from "./lib/base"
-import {
+
+export {
     findChildrenList,
     findChildrenListByFn,
     findParent,
@@ -35,38 +40,3 @@ import {
     isLeafNode,
     findCurrentNodeBrotherNode
 } from "./lib/tree";
-
-export {
-    findChildrenList,
-    findChildrenListByFn,
-    findParent,
-    findPath,
-    findTreeByFlatArray,
-    findTreeByFn,
-    findTreeByTreeData,
-    flattenTree,
-    flattenTreeByDepth,
-    flattenTreeByPostOrder,
-    handlerLinkage,
-    isChildNode,
-    isParentNode,
-    reduceFlattenTree,
-    calculatePercentage,
-    getDistance,
-    isAbleArray,
-    isAbleFn,
-    isAbleObject,
-    isAbleString,
-    isAbleNumber,
-    isHasChange,
-    intersectionArray,
-    symmetricDifference,
-    difference,
-    reviseNumber,
-    stringToNumber,
-    getIntersection,
-    getSymmetricDifference,
-    redoTreeData,
-    isLeafNode,
-    findCurrentNodeBrotherNode
-}
